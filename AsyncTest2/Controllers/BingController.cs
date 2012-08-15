2495a1f1ce389d37fdb45375216e4ea5af6ac5b4
unknown <cchen@CI1220.children.org>
2012-08-15 09:56:03 -0500

    commit

--- AsyncTest2/Controllers/BingController.cs
+++ AsyncTest2/Controllers/BingController.cs
@@ -22,7 +22,8 @@ namespace AsyncTest2.Controllers
         {  
             new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1.39M },  
             new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },  
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }  
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M },
+            new Product { Id = 4, Name = "Hammer2", Category = "Hardware2", Price = 19.99M }  
         }; 
         
         
